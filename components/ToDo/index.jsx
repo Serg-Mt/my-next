c965e4378654c4d36f46250873a9c9cad20ac4ce
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2024-10-21 23:13:33 +0400

    рендеры оптимизированы

--- components/ToDo/index.jsx
+++ components/ToDo/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback, memo, useRef } from 'react';
 
 
 
@@ -6,22 +6,22 @@ export function ToDoApp() {
   const
     [list, setList] = useState([new ListItem('дело 1'), new ListItem('дело 2')]),
 
-    delItem = id => setList(prev => {
+    delItem = useCallback(id => setList(prev => {
       // const
       //   index = prev.findIndex(item => item.id === id);
       // return prev.toSpliced(index, 1)
       return prev.filter(item => item.id !== id);
-    }),
-    toggleCheckbox = id => setList(prev => {
+    }), []),
+    toggleCheckbox = useCallback(id => setList(prev => {
       const
         index = prev.findIndex(item => item.id === id);
-      return prev.with(index, prev[index].toggleChecked());
-    });
-
+      return prev.with(index, prev[index].cloneAndToggleChecked());
+    }), []),
+    addItem = useCallback(text => setList(prev => [...prev, new ListItem(text)]), []);
   return <fieldset>
     <legend>To Do App</legend>
     <Form
-      addItem={text => setList(prev => [...prev, new ListItem(text)])}
+      addItem={addItem}
     />
     <List
       list={list}
@@ -31,28 +31,33 @@ export function ToDoApp() {
   </fieldset>
 }
 
-function Item({ item, delItem, toggleCheckbox }) {
+const Item = memo(function ({ item, delItem, toggleCheckbox }) {
   console.debug('Item', item);
   const
-    { id, checked, text } = item;
+    { id, checked, text } = item,
+    onClick = useCallback(() => delItem(id), [id]);
   return <li>
     <input type="checkbox" checked={checked} onChange={() => toggleCheckbox(id)} />
     {checked && '✔'}
     {text}
-    <Button onClick={() => delItem(id)}>❌</Button>
+    <Button onClick={onClick}>❌</Button>
   </li>
-}
+});
 
-function Form({ addItem }) {
+const Form = memo(function ({ addItem }) {
   console.debug('Form');
   const
-    [value, setValue] = useState('-start-');
+    [value, setValue] = useState('-start-'),
+    ref = useRef(null),
+    onClick = useCallback(() => { addItem(ref.current); /* setValue('') */ }, []);
+  ref.current = value;
+  
   return <fieldset>
     <legend>Form</legend>
     <input value={value} onInput={({ target: { value } }) => setValue(value)} />
-    <Button onClick={() => { addItem(value); setValue('') }}>Add</Button>
+    <Button onClick={onClick}>Add</Button>
   </fieldset>
-}
+});
 
 function List({ list, delItem, toggleCheckbox }) {
   console.debug('List');
@@ -64,12 +69,12 @@ function List({ list, delItem, toggleCheckbox }) {
   </fieldset>
 }
 
-function Button({ onClick, children }) {
-  console.debug('Button');
+const Button = memo(function ({ onClick, children }) {
+  console.debug('Button', children);
   return <button onClick={onClick}>
     {children}
   </button>
-}
+});
 
 class ListItem {
   checked = false;
@@ -78,8 +83,9 @@ class ListItem {
   constructor(text) {
     Object.assign(this, { text });// this.text = text
   }
-  toggleChecked() {
-    this.checked = !this.checked;
-    return this;
+  cloneAndToggleChecked() {
+    const clone = new ListItem;
+    Object.assign(clone, this, { checked: !this.checked });
+    return clone;
   }
 }
